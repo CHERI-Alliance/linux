a7706b1e1b009d299458fc471a0c127e2ccfc081
Kevin Brodsky <kevin.brodsky@arm.com>
2021-04-23 10:18:53 +0100

    linux/user_ptr.h: Introduce user_ptr conversion helpers
    On certain architectures, in particular those implementing the
    pure-capability uABI, the conversion from an integer to a user
    pointer needs to be implemented in a different way depending on the
    nature of the input integer / address.
    
    To allow for this, introduce new *_to_user_ptr* helpers that
    architectures can override, as well as a new as_user_ptr() helper
    for cases where the resulting user pointer is not expected to be
    dereferenceable (useful for instance when storing an arbitrary
    integer in a user pointer). as_user_ptr_strict() additionally checks
    that the input type is as specified.
    
    Conversely, a new user_ptr_addr() helper is added to extract the
    address of a user pointer using the appropriate casts.
    
    u64_to_user_ptr() is reimplemented in terms of as_user_ptr_strict().
    Its usage is deprecated, as the semantics of *_to_user_ptr* helpers
    is now to produce dereferenceable user pointers, unlike
    u64_to_user_ptr().
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -35,6 +35,7 @@
 #include <linux/instruction_pointer.h>
 #include <linux/wordpart.h>
 
+#include <linux/user_ptr.h>
 #include <asm/byteorder.h>
 
 #include <uapi/linux/kernel.h>
@@ -47,12 +48,33 @@
 
 #define PTR_IF(cond, ptr)	((cond) ? (ptr) : NULL)
 
-#define u64_to_user_ptr(x) (		\
-{					\
-	typecheck(u64, (x));		\
-	(void __user *)(uintptr_t)(x);	\
-}					\
-)
+/**
+ * upper_32_bits - return bits 32-63 of a number
+ * @n: the number we're accessing
+ *
+ * A basic shift-right of a 64- or 32-bit quantity.  Use this to suppress
+ * the "right shift count >= width of type" warning when that quantity is
+ * 32-bits.
+ */
+#define upper_32_bits(n) ((u32)(((n) >> 16) >> 16))
+
+/**
+ * lower_32_bits - return bits 0-31 of a number
+ * @n: the number we're accessing
+ */
+#define lower_32_bits(n) ((u32)((n) & 0xffffffff))
+
+/**
+ * upper_16_bits - return bits 16-31 of a number
+ * @n: the number we're accessing
+ */
+#define upper_16_bits(n) ((u16)((n) >> 16))
+
+/**
+ * lower_16_bits - return bits 0-15 of a number
+ * @n: the number we're accessing
+ */
+#define lower_16_bits(n) ((u16)((n) & 0xffff))
 
 struct completion;
 struct user;
