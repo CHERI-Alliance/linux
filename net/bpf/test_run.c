a750c1cf4af8ba4e7b16759e059c4cabd488414f
Zachary Leaf <zachary.leaf@arm.com>
2022-11-02 10:13:25 +0000

    bpf: use user pointer types in uAPI structs
    union bpf_attr and structs bpf_xyz_info are used to pass config to the
    kernel or return info to userspace via the bpf syscall.
    
    In order to avoid needing a separate 32-bit compat handler for the bpf
    syscall, __user pointers inside these union/structs are stored as __u64
    or __aligned_u64.
    
    In PCuABI, a user pointer is a 129-bit capability so __aligned_u64 type
    is not big enough to hold it. Use type __kernel_aligned_uintptr_t
    instead, which is big enough on the affected architectures while
    remaining __aligned_64 on others.
    
    Use copy_{to,from}_user_with_ptr variants where blocks of memory
    containing pointers are being copied to preserve capabilities.
    
    Signed-off-by: Zachary Leaf <zachary.leaf@arm.com>
    Co-developed-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- net/bpf/test_run.c
+++ net/bpf/test_run.c
@@ -439,7 +439,7 @@ static int bpf_test_finish(const union bpf_attr *kattr,
 			   struct skb_shared_info *sinfo, u32 size,
 			   u32 retval, u32 duration)
 {
-	void __user *data_out = u64_to_user_ptr(kattr->test.data_out);
+	void __user *data_out = (void __user *)kattr->test.data_out;
 	int err = -EFAULT;
 	u32 copy_size = size;
 
@@ -646,7 +646,7 @@ BTF_KFUNCS_END(test_sk_check_kfunc_ids)
 static void *bpf_test_init(const union bpf_attr *kattr, u32 user_size,
 			   u32 size, u32 headroom, u32 tailroom)
 {
-	void __user *data_in = u64_to_user_ptr(kattr->test.data_in);
+	void __user *data_in = (void __user *)kattr->test.data_in;
 	void *data;
 
 	if (size < ETH_HLEN || size > PAGE_SIZE - headroom - tailroom)
@@ -743,7 +743,7 @@ int bpf_prog_test_run_raw_tp(struct bpf_prog *prog,
 			     const union bpf_attr *kattr,
 			     union bpf_attr __user *uattr)
 {
-	void __user *ctx_in = u64_to_user_ptr(kattr->test.ctx_in);
+	void __user *ctx_in = (void __user *)kattr->test.ctx_in;
 	__u32 ctx_size_in = kattr->test.ctx_size_in;
 	struct bpf_raw_tp_test_run_info info;
 	int cpu = kattr->test.cpu, err = 0;
@@ -798,8 +798,8 @@ int bpf_prog_test_run_raw_tp(struct bpf_prog *prog,
 
 static void *bpf_ctx_init(const union bpf_attr *kattr, u32 max_size)
 {
-	void __user *data_in = u64_to_user_ptr(kattr->test.ctx_in);
-	void __user *data_out = u64_to_user_ptr(kattr->test.ctx_out);
+	void __user *data_in = (void __user *)kattr->test.ctx_in;
+	void __user *data_out = (void __user *)kattr->test.ctx_out;
 	u32 size = kattr->test.ctx_size_in;
 	void *data;
 	int err;
@@ -831,7 +831,7 @@ static int bpf_ctx_finish(const union bpf_attr *kattr,
 			  union bpf_attr __user *uattr, const void *data,
 			  u32 size)
 {
-	void __user *data_out = u64_to_user_ptr(kattr->test.ctx_out);
+	void __user *data_out = (void __user *)kattr->test.ctx_out;
 	int err = -EFAULT;
 	u32 copy_size = size;
 
@@ -1238,7 +1238,7 @@ int bpf_prog_test_run_xdp(struct bpf_prog *prog, const union bpf_attr *kattr,
 		goto free_data;
 
 	if (unlikely(kattr->test.data_size_in > size)) {
-		void __user *data_in = u64_to_user_ptr(kattr->test.data_in);
+		void __user *data_in = (void __user *)kattr->test.data_in;
 
 		while (size < kattr->test.data_size_in) {
 			struct page *page;
@@ -1495,7 +1495,7 @@ int bpf_prog_test_run_syscall(struct bpf_prog *prog,
 			      const union bpf_attr *kattr,
 			      union bpf_attr __user *uattr)
 {
-	void __user *ctx_in = u64_to_user_ptr(kattr->test.ctx_in);
+	void __user *ctx_in = (void __user *)kattr->test.ctx_in;
 	__u32 ctx_size_in = kattr->test.ctx_size_in;
 	void *ctx = NULL;
 	u32 retval;
