d13c44d3d900db76860fdca07573c55aa7518521
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2024-09-16 13:23:34 +0200

    riscv: bakewell: Add bakewell permission bits
    Define the correct permission bits for bakewell CHERI in the
    riscv architecture code.

---
+++ arch/riscv/include/uapi/asm/cheri.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI__ASM_CHERI_H
+#define _UAPI__ASM_CHERI_H
+
+/*
+ * VMem software-defined capability permission, assigned to
+ * the first software defined permission bit on Bakewell.
+ */
+#define CHERI_PERM_SW_VMEM	(1 << 16)
+
+#endif /* _UAPI__ASM_CHERI_H */
