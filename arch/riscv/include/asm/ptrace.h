02aa6d6dd70df14c3fd4e0dcef3c80a73641e50d
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2024-09-16 15:14:01 +0200

    riscv: bakewell: Prepare register definitions for CHERI
    Adjust arch/riscv/include/asm/ptrace.h for CHERI. This includes
    the definition of struct pt_regs. All capability aware registers
    are capabilities if the PCuABI is in use.

--- arch/riscv/include/asm/ptrace.h
+++ arch/riscv/include/asm/ptrace.h
@@ -12,45 +12,53 @@
 
 #ifndef __ASSEMBLY__
 
+typedef __kernel_uintptr_t register_t;
+
 struct pt_regs {
-	unsigned long epc;
-	unsigned long ra;
-	unsigned long sp;
-	unsigned long gp;
-	unsigned long tp;
-	unsigned long t0;
-	unsigned long t1;
-	unsigned long t2;
-	unsigned long s0;
-	unsigned long s1;
-	unsigned long a0;
-	unsigned long a1;
-	unsigned long a2;
-	unsigned long a3;
-	unsigned long a4;
-	unsigned long a5;
-	unsigned long a6;
-	unsigned long a7;
-	unsigned long s2;
-	unsigned long s3;
-	unsigned long s4;
-	unsigned long s5;
-	unsigned long s6;
-	unsigned long s7;
-	unsigned long s8;
-	unsigned long s9;
-	unsigned long s10;
-	unsigned long s11;
-	unsigned long t3;
-	unsigned long t4;
-	unsigned long t5;
-	unsigned long t6;
+	register_t epc;
+	register_t ra;
+	register_t sp;
+	register_t gp;
+	register_t tp;
+	register_t t0;
+	register_t t1;
+	register_t t2;
+	register_t s0;
+	register_t s1;
+	register_t a0;
+	register_t a1;
+	register_t a2;
+	register_t a3;
+	register_t a4;
+	register_t a5;
+	register_t a6;
+	register_t a7;
+	register_t s2;
+	register_t s3;
+	register_t s4;
+	register_t s5;
+	register_t s6;
+	register_t s7;
+	register_t s8;
+	register_t s9;
+	register_t s10;
+	register_t s11;
+	register_t t3;
+	register_t t4;
+	register_t t5;
+	register_t t6;
+#ifdef CONFIG_CHERI_PURECAP_UABI
+	register_t ddc;
+#endif
+	/* a0 value before the syscall */
+	register_t orig_a0;
 	/* Supervisor/Machine CSRs */
 	unsigned long status;
 	unsigned long badaddr;
 	unsigned long cause;
-	/* a0 value before the syscall */
-	unsigned long orig_a0;
+#ifdef CONFIG_CHERI_PURECAP_UABI
+	unsigned long _pad;
+#endif
 };
 
 #define PTRACE_SYSEMU			0x1f
@@ -67,12 +75,12 @@ struct pt_regs {
 #define MAX_REG_OFFSET offsetof(struct pt_regs, orig_a0)
 
 /* Helpers for working with the instruction pointer */
-static inline unsigned long instruction_pointer(struct pt_regs *regs)
+static inline register_t instruction_pointer(struct pt_regs *regs)
 {
 	return regs->epc;
 }
 static inline void instruction_pointer_set(struct pt_regs *regs,
-					   unsigned long val)
+					   register_t val)
 {
 	regs->epc = val;
 }
@@ -80,40 +88,40 @@ static inline void instruction_pointer_set(struct pt_regs *regs,
 #define profile_pc(regs) instruction_pointer(regs)
 
 /* Helpers for working with the user stack pointer */
-static inline unsigned long user_stack_pointer(struct pt_regs *regs)
+static inline register_t user_stack_pointer(struct pt_regs *regs)
 {
 	return regs->sp;
 }
 static inline void user_stack_pointer_set(struct pt_regs *regs,
-					  unsigned long val)
+					  register_t val)
 {
 	regs->sp =  val;
 }
 
 /* Valid only for Kernel mode traps. */
-static inline unsigned long kernel_stack_pointer(struct pt_regs *regs)
+static inline register_t kernel_stack_pointer(struct pt_regs *regs)
 {
 	return regs->sp;
 }
 
 /* Helpers for working with the frame pointer */
-static inline unsigned long frame_pointer(struct pt_regs *regs)
+static inline register_t frame_pointer(struct pt_regs *regs)
 {
 	return regs->s0;
 }
 static inline void frame_pointer_set(struct pt_regs *regs,
-				     unsigned long val)
+				     register_t val)
 {
 	regs->s0 = val;
 }
 
-static inline unsigned long regs_return_value(struct pt_regs *regs)
+static inline register_t regs_return_value(struct pt_regs *regs)
 {
 	return regs->a0;
 }
 
 static inline void regs_set_return_value(struct pt_regs *regs,
-					 unsigned long val)
+					 register_t val)
 {
 	regs->a0 = val;
 }
@@ -134,13 +142,13 @@ void prepare_ftrace_return(unsigned long *parent, unsigned long self_addr,
  * The @offset is the offset of the register in struct pt_regs.
  * If @offset is bigger than MAX_REG_OFFSET, this returns 0.
  */
-static inline unsigned long regs_get_register(struct pt_regs *regs,
-					      unsigned int offset)
+static inline register_t regs_get_register(struct pt_regs *regs,
+					   unsigned int offset)
 {
 	if (unlikely(offset > MAX_REG_OFFSET))
 		return 0;
 
-	return *(unsigned long *)((unsigned long)regs + offset);
+	return *(register_t *)((register_t)regs + offset);
 }
 
 /**
@@ -153,8 +161,8 @@ static inline unsigned long regs_get_register(struct pt_regs *regs,
  * Note you can get the parameter correctly if the function has no
  * more than eight arguments.
  */
-static inline unsigned long regs_get_kernel_argument(struct pt_regs *regs,
-						unsigned int n)
+static inline register_t regs_get_kernel_argument(struct pt_regs *regs,
+						  unsigned int n)
 {
 	static const int nr_reg_arguments = 8;
 	static const unsigned int argument_offs[] = {
