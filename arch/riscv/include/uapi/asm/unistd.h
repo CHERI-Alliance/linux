68f0732f6a1d29ff739fdb1f0522f14e3f148e9a
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2024-10-01 12:08:15 +0200

    riscv: bakewell: Use new STAT call for CHERI
    The __LP64__ conditional is not defined for the CHERI. However,
    userland expects the new STAT anyway.

--- arch/riscv/include/uapi/asm/unistd.h
+++ arch/riscv/include/uapi/asm/unistd.h
@@ -15,7 +15,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#if defined(__LP64__) && !defined(__SYSCALL_COMPAT)
+/* FIXCHERI: Check condition here. */
+#if (defined(__LP64__) || defined(__CHERI__)) && !defined(__SYSCALL_COMPAT)
 #define __ARCH_WANT_NEW_STAT
 #define __ARCH_WANT_SET_GET_RLIMIT
 #endif /* __LP64__ */
