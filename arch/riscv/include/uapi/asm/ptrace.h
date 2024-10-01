35ef69d69d055bdf09634d8ab61fbd5906b21549
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2024-10-01 14:03:42 +0200

    riscv: bakewell: Adapt sigcontext
    Leave enough space in the signal context to save all registers
    including ddc as capabilities.

--- arch/riscv/include/uapi/asm/ptrace.h
+++ arch/riscv/include/uapi/asm/ptrace.h
@@ -15,6 +15,12 @@
 #define PTRACE_GETFDPIC_EXEC	0
 #define PTRACE_GETFDPIC_INTERP	1
 
+#ifdef __CHERI_PURE_CAPABILITY__
+typedef user_uintptr_t register_t;
+#else
+typedef unsigned long register_t;
+#endif
+
 /*
  * User-mode register state for core dumps, ptrace, sigcontext
  *
@@ -22,38 +28,41 @@
  * struct user_regs_struct must form a prefix of struct pt_regs.
  */
 struct user_regs_struct {
-	unsigned long pc;
-	unsigned long ra;
-	unsigned long sp;
-	unsigned long gp;
-	unsigned long tp;
-	unsigned long t0;
-	unsigned long t1;
-	unsigned long t2;
-	unsigned long s0;
-	unsigned long s1;
-	unsigned long a0;
-	unsigned long a1;
-	unsigned long a2;
-	unsigned long a3;
-	unsigned long a4;
-	unsigned long a5;
-	unsigned long a6;
-	unsigned long a7;
-	unsigned long s2;
-	unsigned long s3;
-	unsigned long s4;
-	unsigned long s5;
-	unsigned long s6;
-	unsigned long s7;
-	unsigned long s8;
-	unsigned long s9;
-	unsigned long s10;
-	unsigned long s11;
-	unsigned long t3;
-	unsigned long t4;
-	unsigned long t5;
-	unsigned long t6;
+	register_t pc;
+	register_t ra;
+	register_t sp;
+	register_t gp;
+	register_t tp;
+	register_t t0;
+	register_t t1;
+	register_t t2;
+	register_t s0;
+	register_t s1;
+	register_t a0;
+	register_t a1;
+	register_t a2;
+	register_t a3;
+	register_t a4;
+	register_t a5;
+	register_t a6;
+	register_t a7;
+	register_t s2;
+	register_t s3;
+	register_t s4;
+	register_t s5;
+	register_t s6;
+	register_t s7;
+	register_t s8;
+	register_t s9;
+	register_t s10;
+	register_t s11;
+	register_t t3;
+	register_t t4;
+	register_t t5;
+	register_t t6;
+#ifdef __CHERI_PURE_CAPABILITY__
+	register_t ddc;
+#endif
 };
 
 struct __riscv_f_ext_state {
