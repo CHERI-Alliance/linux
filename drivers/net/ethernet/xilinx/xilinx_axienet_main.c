7066365bfaec8ea23bed3d03f5010528571c13ab
Stuart Menefy <stuart.menefy@codasip.com>
2024-11-07 15:02:35 +0000

    net: axienet: Don't ignore errors in axienet_open()
    Reintroduce code to check the result of calling axienet_device_reset()
    in axienet_open().
    
    This issue was found when axienet_device_reset() failed because it was
    unable to allocate DMA descriptors, leading to a NULL pointer dereference
    when axienet_start_xmit() called axienet_check_tx_bd_space().
    
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>
    Fixes: 253761a0e61b ("net: xilinx: axiethernet: Enable dynamic MDIO MDC")

--- drivers/net/ethernet/xilinx/xilinx_axienet_main.c
+++ drivers/net/ethernet/xilinx/xilinx_axienet_main.c
@@ -1523,6 +1523,8 @@ static int axienet_open(struct net_device *ndev)
 	axienet_lock_mii(lp);
 	ret = axienet_device_reset(ndev);
 	axienet_unlock_mii(lp);
+	if (ret)
+		return ret;
 
 	ret = phylink_of_phy_connect(lp->phylink, lp->dev->of_node, 0);
 	if (ret) {
