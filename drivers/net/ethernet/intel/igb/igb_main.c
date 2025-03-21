2540b5147e0d6bc3d305811243468f580aabe748
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2025-03-21 16:20:55 +0100

    cheri: Fix compile and warnings in drivers/net/ethernet/
    - Standard uintptr_t vs. unsigned long issues.
    - Some nasty hacks are required in drivers/net/ti/.
    - neterion and 3com drivers must fabricate pointers from
      opaque data in hardware structures.

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -2694,7 +2694,7 @@ static int igb_parse_cls_flower(struct igb_adapter *adapter,
 	}
 
 	input->action = traffic_class;
-	input->cookie = f->cookie;
+	input->cookie = __c_ua(f->cookie);
 
 	return 0;
 }
