39e58ab43dc9cc15e819c34282368882043ff84c
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2025-04-01 17:23:02 +0200

    cheri: Fix compile in drivers/uio/
    - Standard uintptr_t vs. unsigned long issues.

--- drivers/uio/uio.c
+++ drivers/uio/uio.c
@@ -708,7 +708,7 @@ static vm_fault_t uio_vma_fault(struct vm_fault *vmf)
 	 */
 	offset = (vmf->pgoff - mi) << PAGE_SHIFT;
 
-	addr = (void *)(unsigned long)idev->info->mem[mi].addr + offset;
+	addr = (void *)(uintptr_t)idev->info->mem[mi].addr + offset;
 	if (idev->info->mem[mi].memtype == UIO_MEM_LOGICAL)
 		page = virt_to_page(addr);
 	else
@@ -769,7 +769,7 @@ static int uio_mmap_physical(struct vm_area_struct *vma)
 	 */
 	return remap_pfn_range(vma,
 			       vma->vm_start,
-			       mem->addr >> PAGE_SHIFT,
+			       __c_ua(mem->addr) >> PAGE_SHIFT,
 			       vma->vm_end - vma->vm_start,
 			       vma->vm_page_prot);
 }
@@ -843,7 +843,7 @@ static int uio_mmap(struct file *filep, struct vm_area_struct *vma)
 	}
 
 	requested_pages = vma_pages(vma);
-	actual_pages = ((idev->info->mem[mi].addr & ~PAGE_MASK)
+	actual_pages = ((__c_ua(idev->info->mem[mi].addr) & ~PAGE_MASK)
 			+ idev->info->mem[mi].size + PAGE_SIZE -1) >> PAGE_SHIFT;
 	if (requested_pages > actual_pages) {
 		ret = -EINVAL;
