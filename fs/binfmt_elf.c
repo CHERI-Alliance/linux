ba5be9e360ddd287a564a530e1e4b0eeb17c1c5d
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2025-06-11 12:07:03 +0200

    [CHERRY-PICK] fs/binfmt_elf: Enable reservations
    Cherry pick "8cf3f6f64f48 fs/binfmt_elf: Enable reservations":
    | commit 8cf3f6f64f480c2825fc6854cf6dccb9a928514e
    | Author: Kevin Brodsky <kevin.brodsky@arm.com>
    | Date:   Tue Apr 23 18:34:59 2024 +0100
    |
    |     fs/binfmt_elf: Enable reservations
    |
    |     Enable reservations early (before any mapping is created) if the new
    |     process is in PCuABI, and otherwise disable them (the corresponding
    |     mm flag being inherited from the parent).
    |
    |     Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -1302,6 +1302,8 @@ out_free_interp:
 	if (!(current->personality & ADDR_NO_RANDOMIZE) && snapshot_randomize_va_space)
 		current->flags |= PF_RANDOMIZE;
 
+	reserv_mm_set_flag(current->mm, ELF_COMPAT);
+
 	setup_new_exec(bprm);
 
 	/* Do this so that we can load the interpreter, if need be.  We will
