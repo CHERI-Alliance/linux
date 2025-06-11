c3fe6b0151aa1d2213bc28fd8f129baeeb3d6125
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2025-06-11 11:59:15 +0200

    [CHERRY-PICK] linux/user_ptr.h: Introduce user_ptr conversion helpers
    Cherry pick "56c5a1588167 linux/user_ptr.h: Introduce user_ptr conversion helpers":
    | commit 56c5a1588167087704af3016ec2169e19fafb414
    | Author: Kevin Brodsky <kevin.brodsky@arm.com>
    | Date:   Fri Apr 23 10:18:53 2021 +0100
    |
    |     linux/user_ptr.h: Introduce user_ptr conversion helpers
    |
    |     On certain architectures, in particular those implementing the
    |     pure-capability uABI, the conversion from an integer to a user
    |     pointer needs to be implemented in a different way depending on the
    |     nature of the input integer / address.
    |
    |     To allow for this, introduce new *_to_user_ptr* helpers that
    |     architectures can override, as well as a new as_user_ptr() helper
    |     for cases where the resulting user pointer is not expected to be
    |     dereferenceable (useful for instance when storing an arbitrary
    |     integer in a user pointer). as_user_ptr_strict() additionally checks
    |     that the input type is as specified.
    |
    |     Conversely, a new user_ptr_addr() helper is added to extract the
    |     address of a user pointer using the appropriate casts.
    |
    |     u64_to_user_ptr() is reimplemented in terms of as_user_ptr_strict().
    |     Its usage is deprecated, as the semantics of *_to_user_ptr* helpers
    |     is now to produce dereferenceable user pointers, unlike
    |     u64_to_user_ptr().
    |
    |     Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    
    Cherry-pick notes:
    Keep u64_to_user_ptr in its old place and use it to convert something
    of type __u64ptr to a user pointer. The upstream purpose of this macro
    is to silence pointer size warnings when casting an __u64 to a user
    pointer on 32-bit archs. The UAPI should have __u64ptr where this is
    used.

--- include/linux/util_macros.h
+++ include/linux/util_macros.h
@@ -136,16 +136,17 @@
 #define PTR_IF(cond, ptr)	((cond) ? (ptr) : NULL)
 
 /**
- * to_user_ptr - cast a pointer passed as u64 from user space to void __user *
+ * to_user_ptr - cast a pointer passed as __u64ptr from user space
+ * to void __user *. For CHERI the integer value must be a capability.
  * @x: The u64 value from user space, usually via IOCTL
  *
- * to_user_ptr() simply casts a pointer passed as u64 from user space to void
+ * u64_to_user_ptr() simply casts a pointer passed as u64 from user space to void
  * __user * correctly. Using this lets us get rid of all the tiresome casts.
  */
 #define u64_to_user_ptr(x)		\
 ({					\
-	typecheck(u64, (x));		\
-	(void __user *)(uintptr_t)(x);	\
+	typecheck(__u64ptr, (x));	\
+	(void __user *)(user_uintptr_t)(x);	\
 })
 
 /**
