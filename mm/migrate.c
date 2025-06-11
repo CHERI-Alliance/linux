b08a3e5797a78965786c2453c9e4c70fca8d5d35
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2025-06-11 11:59:25 +0200

    [CHERRY-PICK] mm/migrate: Fix the __capability position for double pointers
    Cherry pick "c0e1b2fa943c mm/migrate: Fix the __capability position for double pointers":
    | commit c0e1b2fa943c8571071806f14189ac7f4d6b7db9
    | Author: Amit Daniel Kachhap <amit.kachhap@arm.com>
    | Date:   Tue Oct 27 12:01:55 2020 +0530
    |
    |     mm/migrate: Fix the __capability position for double pointers
    |
    |     CHERI compilers mandatorily need __capability annotations after the
    |     pointer[*] in case of double pointers.
    |
    |     Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -2253,7 +2253,11 @@ static int move_pages_and_store_status(int node,
  */
 static int do_pages_move(struct mm_struct *mm, nodemask_t task_nodes,
 			 unsigned long nr_pages,
+#ifdef CONFIG_CHERI_PURECAP_UABI
+			 const void * __capability * __capability pages,
+#else
 			 const void __user * __user *pages,
+#endif
 			 const int __user *nodes,
 			 int __user *status, int flags)
 {
@@ -2358,7 +2362,11 @@ out:
  * Determine the nodes of an array of pages and store it in an array of status.
  */
 static void do_pages_stat_array(struct mm_struct *mm, unsigned long nr_pages,
+#ifdef CONFIG_CHERI_PURECAP_UABI
+				const void * __capability *pages, int *status)
+#else
 				const void __user **pages, int *status)
+#endif
 {
 	unsigned long i;
 
@@ -2420,7 +2428,11 @@ static int get_compat_pages_array(const void __user *chunk_pages[],
  * a user array of status.
  */
 static int do_pages_stat(struct mm_struct *mm, unsigned long nr_pages,
+#ifdef CONFIG_CHERI_PURECAP_UABI
+			 const void * __capability * __capability pages,
+#else
 			 const void __user * __user *pages,
+#endif
 			 int __user *status)
 {
 #define DO_PAGES_STAT_CHUNK_NR 16UL
@@ -2499,7 +2511,11 @@ out:
  * process.
  */
 static int kernel_move_pages(pid_t pid, unsigned long nr_pages,
+#ifdef CONFIG_CHERI_PURECAP_UABI
+			     const void * __capability * __capability pages,
+#else
 			     const void __user * __user *pages,
+#endif
 			     const int __user *nodes,
 			     int __user *status, int flags)
 {
@@ -2529,7 +2545,11 @@ static int kernel_move_pages(pid_t pid, unsigned long nr_pages,
 }
 
 SYSCALL_DEFINE6(move_pages, pid_t, pid, unsigned long, nr_pages,
+#ifdef CONFIG_CHERI_PURECAP_UABI
+		const void * __capability * __capability, pages,
+#else
 		const void __user * __user *, pages,
+#endif
 		const int __user *, nodes,
 		int __user *, status, int, flags)
 {
