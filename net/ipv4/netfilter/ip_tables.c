fe89763cd67c71671792b25df684875400e0926d
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2025-06-11 11:59:27 +0200

    [CHERRY-PICK] net: Fix __capability annotations in double pointers
    Cherry pick "45376c4416f2 net: Fix __capability annotations in double pointers":
    | commit 45376c4416f248b8402cfc88681c1b25f288860a
    | Author: Amit Daniel Kachhap <amit.kachhap@arm.com>
    | Date:   Tue Oct 27 19:03:47 2020 +0530
    |
    |     net: Fix __capability annotations in double pointers
    |
    |     CHERI compilers mandatorily need __capability annotations after the
    |     pointer[*] in case of double pointers.
    |
    |     Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    |     Co-developed-by: Kevin Brodsky <kevin.brodsky@arm.com>
    |     Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- net/ipv4/netfilter/ip_tables.c
+++ net/ipv4/netfilter/ip_tables.c
@@ -1215,7 +1215,11 @@ struct compat_ipt_replace {
 };
 
 static int
+#ifdef CONFIG_CHERI_PURECAP_UABI
+compat_copy_entry_to_user(struct ipt_entry *e, void * __capability *dstptr,
+#else
 compat_copy_entry_to_user(struct ipt_entry *e, void __user **dstptr,
+#endif
 			  unsigned int *size, struct xt_counters *counters,
 			  unsigned int i)
 {
