bc571013b8626ebcc125197bc5086bb595f770c7
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2025-07-14 13:58:35 +0200

    cheri: kfence: Make mm/kfence/ ready for CHERI
    Use uintptr_t for values that must be able to hold pointer data
    with provenance. Annotate cast that are necessary because of this.

--- mm/kfence/kfence.h
+++ mm/kfence/kfence.h
@@ -74,7 +74,7 @@ struct kfence_metadata {
 	 *
 	 * Invariant: ALIGN_DOWN(addr, PAGE_SIZE) is constant.
 	 */
-	unsigned long addr;
+	uintptr_t addr;
 
 	/*
 	 * The size of the original allocation.
@@ -114,7 +114,7 @@ static inline struct kfence_metadata *addr_to_metadata(unsigned long addr)
 
 	/* The checks do not affect performance; only called from slow-paths. */
 
-	if (!is_kfence_address((void *)addr))
+	if (!is_kfence_address(__c_fakep(addr)))
 		return NULL;
 
 	/*
