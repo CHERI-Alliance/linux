bluebandit21/morello_ebpf

io_uring: Add explicit capability checks
When working in PCuABI, user space pointers are capabilities that carry
metadata such as bounds and read/write permissions. These two are
automatically checked when direct accesses are performed.

The io_uring interface provides a facility to perform asynchronous I/O
operations, and does not always access the data directly, but it relies
on recalling the pages where it's held from the virtual memory. In order
to this, it extracts the address from the capability ignoring the
metadata.

This commit adds explicit capability checks whenever userspace memory
regions pointed by capabilities are accessed indirectly, as
specified in the io_uring section of the PCuABI specification [1].

[1] https://git.morello-project.org/morello/kernel/linux/-/wikis/Morello-pure-capability-kernel-user-Linux-ABI-specification

Signed-off-by: Luca Vizzarro <Luca.Vizzarro@arm.com>
Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

io_uring/net.c
@@ -1146,9 +1146,8 @@ int io_send_zc(struct io_kiocb *req, unsigned int issue_flags)
 		return io_setup_async_addr(req, &__address, issue_flags);
 
 	if (zc->flags & IORING_RECVSEND_FIXED_BUF) {
-		/* TODO [PCuABI] - capability checks for uaccess */
 		ret = io_import_fixed(ITER_SOURCE, &msg.msg_iter, req->imu,
-					user_ptr_addr(zc->buf), zc->len);
+					zc->buf, zc->len);
 		if (unlikely(ret))
 			return ret;
 		msg.sg_from_iter = io_sg_from_iter;

CHERI-Alliance/linux

io_uring/net.c
@@ -1336,9 +1336,8 @@ static int io_send_zc_import(struct io_kiocb *req, struct io_async_msghdr *kmsg)
 	int ret;
 
 	if (sr->flags & IORING_RECVSEND_FIXED_BUF) {
-		/* TODO [PCuABI] - capability checks for uaccess */
 		ret = io_import_fixed(ITER_SOURCE, &kmsg->msg.msg_iter, req->imu,
-					user_ptr_addr(sr->buf), sr->len);
+				      sr->buf, sr->len);
 		if (unlikely(ret))
 			return ret;
 		kmsg->msg.sg_from_iter = io_sg_from_iter;