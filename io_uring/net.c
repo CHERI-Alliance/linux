bluebandit21/morello_ebpf

io_uring: Fix user pointer downcast
Do not cast a user pointer directly to a type that cannot hold a
CHERI capability, use user_ptr_addr() to extract its address.

In a number of places, explicit capability checks will be needed for
full PCuABI support and TODOs have been added accordingly, as the
extracted user address is then used to access user memory.

Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

io_uring/net.c
@@ -1139,8 +1139,9 @@ int io_send_zc(struct io_kiocb *req, unsigned int issue_flags)
 		return io_setup_async_addr(req, &__address, issue_flags);
 
 	if (zc->flags & IORING_RECVSEND_FIXED_BUF) {
+		/* TODO [PCuABI] - capability checks for uaccess */
 		ret = io_import_fixed(ITER_SOURCE, &msg.msg_iter, req->imu,
-					(u64)(uintptr_t)zc->buf, zc->len);
+					user_ptr_addr(zc->buf), zc->len);
 		if (unlikely(ret))
 			return ret;
 		msg.sg_from_iter = io_sg_from_iter;

CHERI-Alliance/linux

io_uring/net.c
@@ -1331,8 +1331,9 @@ static int io_send_zc_import(struct io_kiocb *req, struct io_async_msghdr *kmsg)
 	int ret;
 
 	if (sr->flags & IORING_RECVSEND_FIXED_BUF) {
+		/* TODO [PCuABI] - capability checks for uaccess */
 		ret = io_import_fixed(ITER_SOURCE, &kmsg->msg.msg_iter, req->imu,
-					(u64)(uintptr_t)sr->buf, sr->len);
+					user_ptr_addr(sr->buf), sr->len);
 		if (unlikely(ret))
 			return ret;
 		kmsg->msg.sg_from_iter = io_sg_from_iter;