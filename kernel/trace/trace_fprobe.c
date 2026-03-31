c68c3b9a159326e3d4e1b0c240efa1cd78388e13
Martin Kaiser <martin.kaiser@codasip.com>
2026-03-31 15:46:40 +0200

    trace_fprobe: update for cheri
    Update trace_fprobe for cheri. This is the code that creates fprobes from
    input strings written to /sys/kernel/tracing/dynamic_events.
    
    Treat the stack as an array of capabilities in
    ftrace_regs_get_kernel_stack_nth.
    
    Fix a size check for the shadow stack. On cheri, it is a uintptr_t array.
    
    The fentry_trace_entry_head and fexit_trace_entry_head structs are stored
    in the ringbuffer. Addresses in those structs must be downgraded.
    
    The parsing of dynamic_events' input strings creates a sequence of fetch
    operations which are processed in four stages.
    
    We use a capability in stage 1 where data is read from the stack, from a
    register or from struct task_struct. The metadata aren't used at the
    moment, we're planning to pass them to the generated dynamic events in
    the future.
    
    edata in stage 1 is data that was collected at function entry and stored
    on the shadow stack. When it's processed at function exit, it must be
    read as a uintptr_t array.
    
    Stages 2-4 may access memory at a user-provided address + offset. Our only
    option is to fabricate a capability for each access.
    
    Update the dest parameters of probe_mem_read and ..._user to allow for
    fabricating capabilities. Fix up the callers in uprobe.
    
    Signed-off-by: Martin Kaiser <martin.kaiser@codasip.com>

--- kernel/trace/trace_fprobe.c
+++ kernel/trace/trace_fprobe.c
@@ -81,7 +81,7 @@ static unsigned long tracepoint_user_ip(struct tracepoint_user *tuser)
 	if (!tuser->tpoint)
 		return 0UL;
 
-	return (unsigned long)tuser->tpoint->probestub;
+	return __c_pa(tuser->tpoint->probestub);
 }
 
 static void __tracepoint_user_free(struct tracepoint_user *tuser)
@@ -279,7 +279,7 @@ process_fetch_insn(struct fetch_insn *code, void *rec, void *edata,
 		   void *dest, void *base)
 {
 	struct ftrace_regs *fregs = rec;
-	unsigned long val;
+	uintptr_t val;
 	int ret;
 
 retry:
@@ -299,7 +299,7 @@ retry:
 		val = ftrace_regs_get_argument(fregs, code->param);
 		break;
 	case FETCH_OP_EDATA:
-		val = *(unsigned long *)((unsigned long)edata + code->offset);
+		val = *(uintptr_t *)((uintptr_t)edata + code->offset);
 		break;
 #endif
 	case FETCH_NOP_SYMBOL:	/* Ignore a place holder */
@@ -363,7 +363,7 @@ static nokprobe_inline
 void store_fprobe_entry_data(void *edata, struct trace_probe *tp, struct ftrace_regs *fregs)
 {
 	struct probe_entry_arg *earg = tp->entry_arg;
-	unsigned long val = 0;
+	uintptr_t val = 0;
 	int i;
 
 	if (!earg)
@@ -377,7 +377,7 @@ void store_fprobe_entry_data(void *edata, struct trace_probe *tp, struct ftrace_
 			val = ftrace_regs_get_argument(fregs, code->param);
 			break;
 		case FETCH_OP_ST_EDATA:
-			*(unsigned long *)((unsigned long)edata + code->offset) = val;
+			*(uintptr_t *)((uintptr_t)edata + code->offset) = val;
 			break;
 		case FETCH_OP_END:
 			goto end;
@@ -1318,7 +1318,7 @@ static int trace_fprobe_create_internal(int argc, const char *argv[],
 			sbuf = kmalloc(KSYM_NAME_LEN, GFP_KERNEL);
 			if (!sbuf)
 				return -ENOMEM;
-			ctx->funcname = kallsyms_lookup((unsigned long)tpoint->probestub,
+			ctx->funcname = kallsyms_lookup(__c_pa(tpoint->probestub),
 							NULL, NULL, NULL, sbuf);
 		}
 	}
@@ -1368,7 +1368,7 @@ static int trace_fprobe_create_internal(int argc, const char *argv[],
 	if (is_return && tf->tp.entry_arg) {
 		tf->fp.entry_handler = trace_fprobe_entry_handler;
 		tf->fp.entry_data_size = traceprobe_get_entry_data_size(&tf->tp);
-		if (ALIGN(tf->fp.entry_data_size, sizeof(long)) > MAX_FPROBE_DATA_SIZE) {
+		if (ALIGN(tf->fp.entry_data_size, RET_STACK_WORD_SIZE) > MAX_FPROBE_DATA_SIZE) {
 			trace_probe_log_set_index(2);
 			trace_probe_log_err(0, TOO_MANY_EARGS);
 			return -E2BIG;
