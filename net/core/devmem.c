bc9da4ccbe77aaf8ea09f6b0c9252c4269939f50
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:38:11 +0200

    cheri: net: Silence remaining CHERI related warnings in net/
    Annotate the reminaing casts as necessary.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- net/core/devmem.c
+++ net/core/devmem.c
@@ -426,7 +426,7 @@ out_err:
 
 struct net_iov *
 net_devmem_get_niov_at(struct net_devmem_dmabuf_binding *binding,
-		       size_t virt_addr, size_t *off, size_t *size)
+		       user_uintptr_t virt_addr, size_t *off, size_t *size)
 {
 	if (virt_addr >= binding->dmabuf->size)
 		return NULL;
@@ -434,7 +434,7 @@ net_devmem_get_niov_at(struct net_devmem_dmabuf_binding *binding,
 	*off = virt_addr % PAGE_SIZE;
 	*size = PAGE_SIZE - *off;
 
-	return binding->tx_vec[virt_addr / PAGE_SIZE];
+	return binding->tx_vec[__c_ua(virt_addr) / PAGE_SIZE];
 }
 
 /*** "Dmabuf devmem memory provider" ***/
