c0b763c6c967bc523655139c42665c681756db88
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:55:13 +0200

    [CHERRY-PICK] mm/mmap: Handle reservations in get_unmapped_area
    Cherry pick "390d38d3f7b4 mm/mmap: Handle reservations in get_unmapped_area":
    | commit 390d38d3f7b46ef8efdeef21fe30efe17acb2d6f
    | Author: Amit Daniel Kachhap <amitdaniel.kachhap@arm.com>
    | Date:   Wed Sep 20 13:05:46 2023 +0530
    |
    |     mm/mmap: Handle reservations in get_unmapped_area
    |
    |     In CHERI architectures, all ranges cannot be represented as capability bounds
    |     so add the necessary CHERI base and length alignment checks when generating
    |     free unmapped virtual address ranges or evaluating a fixed range.
    |
    |     The PCuABI reservation interface stores the unusable alignment gaps at
    |     the start and end. These gaps should be considered when finding the free
    |     unmapped address space.
    |
    |     In the MAP_FIXED case, the requested address range should completely
    |     reside within the reservation range or not overlap with any existing
    |     reservation range.
    |
    |     Signed-off-by: Amit Daniel Kachhap <amitdaniel.kachhap@arm.com>
    |     Co-developed-by: Kevin Brodsky <kevin.brodsky@arm.com>
    |     Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -37,6 +37,7 @@
 #include <linux/bitmap.h>
 #include <linux/bitops.h>
 #include <linux/iommu-debug-pagealloc.h>
+#include <linux/mm_reserv.h>
 
 struct mempolicy;
 struct anon_vma;
@@ -4233,7 +4234,7 @@ static inline unsigned long stack_guard_start_gap(const struct vm_area_struct *v
 static inline unsigned long vm_start_gap(const struct vm_area_struct *vma)
 {
 	unsigned long gap = stack_guard_start_gap(vma);
-	unsigned long vm_start = vma->vm_start;
+	unsigned long vm_start = reserv_vma_reserv_start(vma);
 
 	vm_start -= gap;
 	if (vm_start > vma->vm_start)
@@ -4243,7 +4244,7 @@ static inline unsigned long vm_start_gap(const struct vm_area_struct *vma)
 
 static inline unsigned long vm_end_gap(const struct vm_area_struct *vma)
 {
-	unsigned long vm_end = vma->vm_end;
+	unsigned long vm_end = reserv_vma_reserv_start(vma) + reserv_vma_reserv_len(vma);
 
 	if (vma->vm_flags & VM_GROWSUP) {
 		vm_end += stack_guard_gap;
