59170fcbd8ad0652998c25559b6eec4a859b3ff6
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:37:41 +0200

    cheri: net: Use uintptr_t for some cookies
    This allows the caller that passes in the cookie to case the value
    back to a pointer, e.g. in a callback function.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- net/sched/cls_flower.c
+++ net/sched/cls_flower.c
@@ -2623,7 +2623,7 @@ static void fl_walk(struct tcf_proto *tp, struct tcf_walker *arg,
 		    bool rtnl_held)
 {
 	struct cls_fl_head *head = fl_head_dereference(tp);
-	unsigned long id = arg->cookie, tmp;
+	unsigned long id = __c_ua(arg->cookie), tmp;
 	struct cls_fl_filter *f;
 
 	arg->count = arg->skip;
@@ -2646,7 +2646,7 @@ static void fl_walk(struct tcf_proto *tp, struct tcf_walker *arg,
 		rcu_read_lock();
 	}
 	rcu_read_unlock();
-	arg->cookie = id;
+	arg->cookie = __c_fakeu(id);
 }
 
 static struct cls_fl_filter *
