ebe981b032686b3ab7a00c3f98c7214739760bc4
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:36:29 +0200

    cheri: Fix remaining warnings in lib/
    Fix the remaining uintptr_t related warnings in lib/.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- lib/find_bit_benchmark.c
+++ lib/find_bit_benchmark.c
@@ -43,7 +43,8 @@ static int __init test_find_first_bit(const void *bitmap, unsigned long len)
 	time = ktime_get();
 	for (cnt = i = 0; i < len; cnt++) {
 		i = find_first_bit(cp, len);
-		__clear_bit(i, cp);
+		if (i < len)
+			__clear_bit(i, cp);
 	}
 	time = ktime_get() - time;
 	pr_err("find_first_bit:     %18llu ns, %6ld iterations\n", time, cnt);
@@ -63,7 +64,8 @@ static int __init test_find_first_and_bit(const void *bitmap, const void *bitmap
 	time = ktime_get();
 	for (cnt = i = 0; i < len; cnt++) {
 		i = find_first_and_bit(cp, bitmap2, len);
-		__clear_bit(i, cp);
+		if (i < len)
+			__clear_bit(i, cp);
 	}
 	time = ktime_get() - time;
 	pr_err("find_first_and_bit: %18llu ns, %6ld iterations\n", time, cnt);
