85e8c9ef4879f5770108c202833580398e79d208
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:37:08 +0200

    cheri: fs: afs: Fix uintptr_t usage in fs/afs/ and rxrpc
    Fix uintptr_t usage in fs/afs/ and the related net/rxrpc.
    
    Use uintptr_t for values that can contain a pointer in fs/afs/
    and cast as necessary:
    - The ->d_fsdata field stores the data version which is not a pointer.
    - The procfs callbacks pass the file offset as the iterator.
    - The call_user_ID parameter to verious functions must be able to hold
      a pointer.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- fs/afs/internal.h
+++ fs/afs/internal.h
@@ -1773,8 +1773,7 @@ static inline void afs_update_dentry_version(struct afs_operation *op,
 					     struct dentry *dentry)
 {
 	if (!op->cumul_error.error)
-		dentry->d_fsdata =
-			(void *)(unsigned long)dir_vp->scb.status.data_version;
+		dentry->d_fsdata = __c_fakep(dir_vp->scb.status.data_version);
 }
 
 /*
