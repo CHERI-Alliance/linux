bluebandit21/morello_ebpf

arm64: add 64-bit support to compat helpers
Make is_compat_task() and in_compat_syscall() return True for 64-bit
compat tasks. This is necessary to ensure that 64-bit tasks are treated
correctly as part of the compat layer (e.g. compat_ptr() is called).

Signed-off-by: Kristina Martsenko <kristina.martsenko@arm.com>

arch/arm64/include/asm/compat.h
@@ -100,15 +100,20 @@ static inline void __user *compat_ptr(compat_uptr_t uptr)
 #define COMPAT_USE_64BIT_TIME	1
 #endif
 
+static inline int is_32bit_compat_task(void)
+{
+	return IS_ENABLED(CONFIG_COMPAT32) && test_thread_flag(TIF_32BIT);
+}
+
 static inline int is_compat_task(void)
 {
-	return test_thread_flag(TIF_32BIT);
+	return (IS_ENABLED(CONFIG_COMPAT32) && test_thread_flag(TIF_32BIT)) ||
+	       (IS_ENABLED(CONFIG_COMPAT64) && test_thread_flag(TIF_64BIT_COMPAT));
 }
-#define is_32bit_compat_task is_compat_task
 
 static inline int is_compat_thread(struct thread_info *thread)
 {
-	return test_ti_thread_flag(thread, TIF_32BIT);
+	return IS_ENABLED(CONFIG_COMPAT32) && test_ti_thread_flag(thread, TIF_32BIT);
 }
 
 long compat_arm_syscall(struct pt_regs *regs, int scno);

CHERI-Alliance/linux

(cherry picked from commit ead4635011825786f3b97d17029d07db3b82c1a9)
[ Christian A. Ehrhardt: Directly use final names ]
Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

arch/arm64/include/asm/compat.h
@@ -100,15 +100,20 @@ static inline void __user *compat_ptr(compat_uptr_t uptr)
 #define COMPAT_USE_64BIT_TIME	1
 #endif
 
+static inline int is_compat32_task(void)
+{
+	return IS_ENABLED(CONFIG_COMPAT32) && test_thread_flag(TIF_32BIT);
+}
+
 static inline int is_compat_task(void)
 {
-	return test_thread_flag(TIF_32BIT);
+	return (IS_ENABLED(CONFIG_COMPAT32) && test_thread_flag(TIF_32BIT)) ||
+	       (IS_ENABLED(CONFIG_COMPAT64) && test_thread_flag(TIF_64BIT_COMPAT));
 }
-#define is_compat32_task is_compat_task
 
 static inline int is_compat_thread(struct thread_info *thread)
 {
-	return test_ti_thread_flag(thread, TIF_32BIT);
+	return IS_ENABLED(CONFIG_COMPAT32) && test_ti_thread_flag(thread, TIF_32BIT);
 }
 
 long compat_arm_syscall(struct pt_regs *regs, int scno);