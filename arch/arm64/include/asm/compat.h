bluebandit21/morello_ebpf

arch: add compat helpers specific to 32-bit
The in_compat_syscall() and is_compat_task() functions are currently
used to check two different things:

  1) whether a task uses the compat layer (meaning, for example, that
  compat_ptr() needs to be called)
  2) whether a task is 32-bit

As the kernel is gaining support for 64-bit compat tasks, new functions
are needed to differentiate between these two cases. So add
in_32bit_compat_syscall() and is_32bit_compat_task(). Note that the
64-bit counterparts (in_64bit_compat_syscall() and
is_64bit_compat_task()) are currently not needed.

No functional change at this point.

Signed-off-by: Kristina Martsenko <kristina.martsenko@arm.com>

arch/arm64/include/asm/compat.h
@@ -104,6 +104,7 @@ static inline int is_compat_task(void)
 {
 	return test_thread_flag(TIF_32BIT);
 }
+#define is_32bit_compat_task is_compat_task
 
 static inline int is_compat_thread(struct thread_info *thread)
 {

arch/mips/include/asm/compat.h
@@ -154,5 +154,6 @@ static inline int is_compat_task(void)
 {
 	return test_thread_flag(TIF_32BIT_ADDR);
 }
+#define is_32bit_compat_task is_compat_task
 
 #endif /* _ASM_COMPAT_H */

arch/parisc/include/asm/compat.h
@@ -137,5 +137,6 @@ static inline int is_compat_task(void)
 {
 	return __is_compat_task(current);
 }
+#define is_32bit_compat_task is_compat_task
 
 #endif /* _ASM_PARISC_COMPAT_H */

arch/powerpc/include/asm/compat.h
@@ -109,6 +109,7 @@ static inline int is_compat_task(void)
 {
 	return is_32bit_task();
 }
+#define is_32bit_compat_task is_compat_task
 
 #endif /* __KERNEL__ */
 #endif /* _ASM_POWERPC_COMPAT_H */

arch/sparc/include/asm/compat.h
@@ -148,6 +148,7 @@ static inline int is_compat_task(void)
 {
 	return test_thread_flag(TIF_32BIT);
 }
+#define is_32bit_compat_task is_compat_task
 
 static inline bool in_compat_syscall(void)
 {
@@ -155,6 +156,7 @@ static inline bool in_compat_syscall(void)
 	return pt_regs_trap_type(current_pt_regs()) == 0x110;
 }
 #define in_compat_syscall in_compat_syscall
+#define in_32bit_compat_syscall in_compat_syscall
 #endif
 
 #endif /* _ASM_SPARC64_COMPAT_H */

arch/x86/include/asm/compat.h
@@ -100,6 +100,7 @@ static inline bool in_compat_syscall(void)
 	return in_32bit_syscall();
 }
 #define in_compat_syscall in_compat_syscall	/* override the generic impl */
+#define in_32bit_compat_syscall in_compat_syscall
 #define compat_need_64bit_alignment_fixup in_ia32_syscall
 #endif
 

CHERI-Alliance/linux

in_compat32_syscall() and is_compat32_task(). Note that the
64-bit counterparts (is_compat64_syscall() and
is_compat64_task()) are currently not needed.
(cherry picked from commit 0f81c0f3115954086c702122f03cdbdc81829664)
[ Christian A. Ehrhardt: Directly use the final names ]
Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

arch/arm64/include/asm/compat.h
@@ -104,6 +104,7 @@ static inline int is_compat_task(void)
 {
 	return test_thread_flag(TIF_32BIT);
 }
+#define is_compat32_task is_compat_task
 
 static inline int is_compat_thread(struct thread_info *thread)
 {

arch/mips/include/asm/compat.h
@@ -154,5 +154,6 @@ static inline int is_compat_task(void)
 {
 	return test_thread_flag(TIF_32BIT_ADDR);
 }
+#define is_compat32_task is_compat_task
 
 #endif /* _ASM_COMPAT_H */

arch/parisc/include/asm/compat.h
@@ -137,5 +137,6 @@ static inline int is_compat_task(void)
 {
 	return __is_compat_task(current);
 }
+#define is_compat32_task is_compat_task
 
 #endif /* _ASM_PARISC_COMPAT_H */

arch/powerpc/include/asm/compat.h
@@ -109,6 +109,7 @@ static inline int is_compat_task(void)
 {
 	return is_32bit_task();
 }
+#define is_compat32_task is_compat_task
 
 #endif /* __KERNEL__ */
 #endif /* _ASM_POWERPC_COMPAT_H */

arch/sparc/include/asm/compat.h
@@ -148,6 +148,7 @@ static inline int is_compat_task(void)
 {
 	return test_thread_flag(TIF_32BIT);
 }
+#define is_compat32_task is_compat_task
 
 static inline bool in_compat_syscall(void)
 {
@@ -155,6 +156,7 @@ static inline bool in_compat_syscall(void)
 	return pt_regs_trap_type(current_pt_regs()) == 0x110;
 }
 #define in_compat_syscall in_compat_syscall
+#define in_compat32_syscall in_compat_syscall
 #endif
 
 #endif /* _ASM_SPARC64_COMPAT_H */

arch/x86/include/asm/compat.h
@@ -100,6 +100,7 @@ static inline bool in_compat_syscall(void)
 	return in_32bit_syscall();
 }
 #define in_compat_syscall in_compat_syscall	/* override the generic impl */
+#define in_compat32_syscall in_compat_syscall
 #define compat_need_64bit_alignment_fixup in_ia32_syscall
 #endif
 
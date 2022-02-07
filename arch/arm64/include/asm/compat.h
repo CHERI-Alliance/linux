da7ba8b6f3183448b996cf7e441048a0d7364b30
Kristina Martsenko <kristina.martsenko@arm.com>
2022-02-07 17:36:28 +0000

    arch: add compat helpers specific to 32-bit
    The in_compat_syscall() and is_compat_task() functions are currently
    used to check two different things:
    
      1) whether a task uses the compat layer (meaning, for example, that
      compat_ptr() needs to be called)
      2) whether a task is 32-bit
    
    As the kernel is gaining support for 64-bit compat tasks, new functions
    are needed to differentiate between these two cases. So add
    in_compat32_syscall() and is_compat32_task(). Note that the
    64-bit counterparts (is_compat64_syscall() and
    is_compat64_task()) are currently not needed.
    
    No functional change at this point.
    
    Signed-off-by: Kristina Martsenko <kristina.martsenko@arm.com>
    (cherry picked from commit 0f81c0f3115954086c702122f03cdbdc81829664)
    [ Christian A. Ehrhardt: Directly use the final names ]
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- arch/arm64/include/asm/compat.h
+++ arch/arm64/include/asm/compat.h
@@ -104,6 +104,7 @@ static inline int is_compat_task(void)
 {
 	return test_thread_flag(TIF_32BIT);
 }
+#define is_compat32_task is_compat_task
 
 static inline int is_compat_thread(struct thread_info *thread)
 {
